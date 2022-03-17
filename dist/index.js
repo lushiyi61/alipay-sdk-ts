0a8728ce8192d926e3748d6862b098e1dd5e28fe
suxiaomo <lksuxiaomo@outlook.com>
2022-03-17 14:45:00 +0800

    发布包，占用名称

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,10 @@
-
-
+"use strict";
 // export class AlipaySdk {
 //     private sdkVersion: string;
 //     public config: AlipaySdkConfig;
-
 //     constructor(config: AlipaySdkConfig) {
 //         if (!config.appId) { throw Error('config.appId is required'); }
 //         if (!config.privateKey) { throw Error('config.privateKey is required'); }
-
 //         const privateKeyType = config.keyType === 'PKCS8' ? 'PRIVATE KEY' : 'RSA PRIVATE KEY';
 //         config.privateKey = this.formatKey(config.privateKey, privateKeyType);
 //         // 普通公钥模式和证书模式二选其一，传入了证书路径或内容认为是证书模式
@@ -38,25 +35,19 @@
 //             charset: 'utf-8',
 //             version: '1.0',
 //         }, camelcaseKeys(config, { deep: true }));
-
 //         this.sdkVersion = `alipay-sdk-nodejs-${pkg.version}`;
 //     }
-
 //     // 格式化 key
 //     private formatKey(key: string, type: string): string {
 //         const item = key.split('\n').map(val => val.trim());
-
 //         // 删除包含 `RSA PRIVATE KEY / PUBLIC KEY` 等字样的第一行
 //         if (item[0].includes(type)) { item.shift(); }
-
 //         // 删除包含 `RSA PRIVATE KEY / PUBLIC KEY` 等字样的最后一行
 //         if (item[item.length - 1].includes(type)) {
 //             item.pop();
 //         }
-
 //         return `-----BEGIN ${type}-----\n${item.join('')}\n-----END ${type}-----`;
 //     }
-
 //     // 格式化请求 url（按规范把某些固定的参数放入 url）
 //     private formatUrl(url: string, params: { [key: string]: string }): { execParams: any, url: string } {
 //         let requestUrl = url;
@@ -68,7 +59,6 @@
 //             'app_cert_sn', 'alipay_root_cert_sn',
 //             'ws_service_url',
 //         ];
-
 //         for (const key in params) {
 //             if (urlArgs.indexOf(key) > -1) {
 //                 const val = encodeURIComponent(params[key]);
@@ -77,10 +67,8 @@
 //                 delete params[key];
 //             }
 //         }
-
 //         return { execParams: params, url: requestUrl };
 //     }
-
 //     // 文件上传
 //     private multipartExec(method: string, option: IRequestOption = {}): Promise<AlipaySdkCommonResult> {
 //         const config = this.config;
@@ -88,33 +76,26 @@
 //         let formData = {} as { [key: string]: string | Object | fs.ReadStream };
 //         const infoLog = (option.log && is.fn(option.log.info)) ? option.log.info : null;
 //         const errorLog = (option.log && is.fn(option.log.error)) ? option.log.error : null;
-
 //         option.formData.getFields().forEach((field) => {
 //             // 字段加入签名参数（文件不需要签名）
 //             signParams[field.name] = field.value;
 //             formData[field.name] = field.value;
 //         });
-
 //         // 签名方法中使用的 key 是驼峰
 //         signParams = camelcaseKeys(signParams, { deep: true });
-
 //         formData = snakeCaseKeys(formData);
-
 //         option.formData.getFiles().forEach((file) => {
 //             // 文件名需要转换驼峰为下划线
 //             const fileKey = decamelize(file.fieldName);
 //             // 单独处理文件类型
 //             formData[fileKey] = !isuri.isValid(file.path) ? fs.createReadStream(file.path) : request(file.path);
 //         });
-
 //         // 计算签名
 //         const signData = sign(method, signParams, config);
 //         // 格式化 url
 //         const { url } = this.formatUrl(config.gateway, signData);
-
 //         infoLog && infoLog('[AlipaySdk]start exec url: %s, method: %s, params: %s',
 //             url, method, JSON.stringify(signParams));
-
 //         return new Promise((resolve, reject) => {
 //             request.post({
 //                 url,
@@ -129,16 +110,13 @@
 //                     errorLog && errorLog(err);
 //                     return reject(err);
 //                 }
-
 //                 infoLog && infoLog('[AlipaySdk]exec response: %s', body);
-
 //                 try {
 //                     let data;
 //                     let responseKey;
 //                     const result = JSON.parse(body);
 //                     responseKey = `${method.replace(/\./g, '_')}_response`;
 //                     data = result[responseKey];
-
 //                     // 开放平台返回错误时，`${responseKey}` 对应的值不存在
 //                     if (data) {
 //                         // 验签
@@ -151,46 +129,37 @@
 //                 } catch (e) {
 //                     return reject({ serverResult: body, errorMessage: '[AlipaySdk]Response 格式错误' });
 //                 }
-
 //                 return reject({ serverResult: body, errorMessage: '[AlipaySdk]HTTP 请求错误' });
 //             });
 //         });
 //     }
-
 //     // page 类接口
 //     public pageExec(method: string, option: IRequestOption = {}): Promise<string> {
 //         let signParams = { alipaySdk: this.sdkVersion } as { [key: string]: string | Object };
 //         const config = this.config;
 //         const infoLog = (option.log && is.fn(option.log.info)) ? option.log.info : null;
-
 //         // option.formData.getFields().forEach((field) => {
 //         //   signParams[field.name] = field.value;
 //         // });
 //         Object.keys(option.formData).map(key => {
 //             signParams[key] = option.formData[key]
 //         })
-
 //         // 签名方法中使用的 key 是驼峰
 //         signParams = camelcaseKeys(signParams, { deep: true });
-
 //         // 计算签名
 //         const signData = sign(method, signParams, config);
 //         // 格式化 url
 //         const { url, execParams } = this.formatUrl(config.gateway, signData);
-
 //         infoLog && infoLog('[AlipaySdk]start exec url: %s, method: %s, params: %s',
 //             url, method, JSON.stringify(signParams));
-
 //         if (1) {
 //             return new Promise((resolve) => {
 //                 const query = Object.keys(execParams).map((key) => {
 //                     return `${key}=${encodeURIComponent(execParams[key])}`;
 //                 });
-
 //                 resolve(`${url}&${query.join('&')}`);
 //             });
 //         }
-
 //         return new Promise((resolve) => {
 //             // 生成表单
 //             const formName = `alipaySDKSubmit${Date.now()}`;
@@ -205,12 +174,10 @@
 //       `);
 //         });
 //     }
-
 //     // 消息验签
 //     private notifyRSACheck(signArgs: { [key: string]: any }, signStr: string, signType: 'RSA' | 'RSA2', raw?: boolean) {
 //         const signContent = Object.keys(signArgs).sort().filter(val => val).map((key) => {
 //             let value = signArgs[key];
-
 //             if (Array.prototype.toString.call(value) !== '[object String]') {
 //                 value = JSON.stringify(value);
 //             }
@@ -221,12 +188,9 @@
 //             }
 //             return `${key}=${decodeURIComponent(value)}`;
 //         }).join('&');
-
 //         const verifier = crypto.createVerify(ALIPAY_ALGORITHM_MAPPING[signType]);
-
 //         return verifier.update(signContent, 'utf8').verify(this.config.alipayPublicKey, signStr, 'base64');
 //     }
-
 //     /**
 //      *
 //      * @param originStr 开放平台返回的原始字符串
@@ -239,7 +203,6 @@
 //         const startIndex = originStr.indexOf(`${responseKey}"`);
 //         // 找到最后一个 “"sign"” 字符串的位置（避免）
 //         const lastIndex = originStr.lastIndexOf('"sign"');
-
 //         /**
 //          * 删除 xxx_response 及之前的字符串
 //          * 假设原始字符串为
@@ -248,7 +211,6 @@
 //          *  :{"code":"10000"},"sign":"jumSvxTKwn24G5sAIN"}
 //          */
 //         validateStr = validateStr.substr(startIndex + responseKey.length + 1);
-
 //         /**
 //          * 删除最后一个 "sign" 及之后的字符串
 //          * 删除后变为
@@ -256,13 +218,10 @@
 //          * {} 之间就是待验签的字符串
 //          */
 //         validateStr = validateStr.substr(0, lastIndex);
-
 //         // 删除第一个 { 之前的任何字符
 //         validateStr = validateStr.replace(/^[^{]*{/g, '{');
-
 //         // 删除最后一个 } 之后的任何字符
 //         validateStr = validateStr.replace(/\}([^}]*)$/g, '}');
-
 //         return validateStr;
 //     }
 //     exec<T = {}>(
@@ -294,24 +253,20 @@
 //             if (option.formData.getFiles().length > 0) {
 //                 return this.multipartExec(method, option);
 //             }
-
 //             /**
 //              * fromData 中不包含文件时，认为是 page 类接口（返回 form 表单）
 //              * 比如 PC 端支付接口 alipay.trade.page.pay
 //              */
 //             return this.pageExec(method, option);
 //         }
-
 //         const config = this.config;
 //         // 计算签名
 //         const signData = sign(method, params, config);
 //         const { url, execParams } = this.formatUrl(config.gateway, signData);
 //         const infoLog = (option.log && is.fn(option.log.info)) ? option.log.info : null;
 //         const errorLog = (option.log && is.fn(option.log.error)) ? option.log.error : null;
-
 //         infoLog && infoLog('[AlipaySdk]start exec, url: %s, method: %s, params: %s',
 //             url, method, JSON.stringify(execParams));
-
 //         return new Promise((resolve, reject) => {
 //             config.urllib.request(url, {
 //                 method: 'POST',
@@ -323,7 +278,6 @@
 //             })
 //                 .then((ret: { status: number, data: string }) => {
 //                     infoLog && infoLog('[AlipaySdk]exec response: %s', ret);
-
 //                     if (ret.status === 200) {
 //                         /**
 //                          * 示例响应格式
@@ -345,24 +299,19 @@
 //                         } catch (e) {
 //                             return reject({ serverResult: ret, errorMessage: '[AlipaySdk]Response 格式错误' });
 //                         }
-
 //                         if (data) {
 //                             if (params.needEncrypt) {
 //                                 data = aesDecrypt(data, config.encryptKey);
 //                             }
-
 //                             // 按字符串验签
 //                             const validateSuccess = option.validateSign ? this.checkResponseSign(ret.data, responseKey) : true;
-
 //                             if (validateSuccess) {
 //                                 return resolve(config.camelcase ? camelcaseKeys(data, { deep: true }) : data);
 //                             }
 //                             return reject({ serverResult: ret, errorMessage: '[AlipaySdk]验签失败' });
 //                         }
-
 //                         return reject({ serverResult: ret, errorMessage: '[AlipaySdk]HTTP 请求错误' });
 //                     }
-
 //                     reject({ serverResult: ret, errorMessage: '[AlipaySdk]HTTP 请求错误' });
 //                 })
 //                 .catch((err) => {
@@ -372,7 +321,6 @@
 //                 });
 //         });
 //     }
-
 //     // 结果验签
 //     checkResponseSign(signStr: string, responseKey: string): boolean {
 //         if (!this.config.alipayPublicKey || this.config.alipayPublicKey === '') {
@@ -380,21 +328,17 @@
 //             // 支付宝公钥不存在时不做验签
 //             return true;
 //         }
-
 //         // 带验签的参数不存在时返回失败
 //         if (!signStr) { return false; }
-
 //         // 根据服务端返回的结果截取需要验签的目标字符串
 //         const validateStr = this.getSignStr(signStr, responseKey);
 //         // 服务端返回的签名
 //         const serverSign = JSON.parse(signStr).sign;
-
 //         // 参数存在，并且是正常的结果（不包含 sub_code）时才验签
 //         const verifier = crypto.createVerify(ALIPAY_ALGORITHM_MAPPING[this.config.signType]);
 //         verifier.update(validateStr, 'utf8');
 //         return verifier.verify(this.config.alipayPublicKey, serverSign, 'base64');
 //     }
-
 //     /**
 //      * 通知验签
 //      * @param postData {JSON} 服务端的消息内容
@@ -402,28 +346,23 @@
 //      */
 //     checkNotifySign(postData: any, raw?: boolean): boolean {
 //         const signStr = postData.sign;
-
 //         // 未设置“支付宝公钥”或签名字符串不存，验签不通过
 //         if (!this.config.alipayPublicKey || !signStr) {
 //             return false;
 //         }
-
 //         // 先从签名字符串中取 sign_type，再取配置项、都不存在时默认为 RSA2（RSA 已不再推荐使用）
 //         const signType = postData.sign_type || this.config.signType || 'RSA2';
 //         const signArgs = { ...postData };
 //         // 除去 sign
 //         delete signArgs.sign;
-
 //         /**
 //          * 某些用户可能自己删除了 sign_type 后再验签
 //          * 为了保持兼容性临时把 sign_type 加回来
 //          * 因为下面的逻辑会验签 2 次所以不会存在验签不同过的情况
 //          */
 //         signArgs.sign_type = signType;
-
 //         // 保留 sign_type 验证一次签名
 //         const verifyResult = this.notifyRSACheck(signArgs, signStr, signType, raw);
-
 //         if (!verifyResult) {
 //             /**
 //              * 删除 sign_type 验一次
@@ -433,7 +372,6 @@
 //             delete signArgs.sign_type;
 //             return this.notifyRSACheck(signArgs, signStr, signType, raw);
 //         }
-
 //         return true;
 //     }
 // }
